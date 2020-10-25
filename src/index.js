71bdeaf73344fe173cc22a8d671049230aba7e8a
Nataliia Ometiukh <nataliia.ometiukh@gmail.com>
2020-10-25 23:25:24 +0200

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let arrOnes = [
+        '',
+        ' one',
+        ' two',
+        ' three',
+        ' four',
+        ' five',
+        ' six',
+        ' seven',
+        ' eight',
+        ' nine',
+        ' ten',
+        ' eleven',
+        ' twelve',
+        ' thirteen',
+        ' fourteen',
+        ' fifteen',
+        ' sixteen',
+        ' seventeen',
+        ' eighteen',
+        ' nineteen'
+    ];
+    let arrTens = [
+        '',
+        '',
+        ' twenty',
+        ' thirty',
+        ' forty',
+        ' fifty',
+        ' sixty',
+        ' seventy',
+        ' eighty',
+        ' ninety'
+    ];
+    let hundred = ' hundred';
+    let result = '';
+    let numStr = number.toString();
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (numStr.length === 3) {
+        result = arrOnes[parseInt(numStr.charAt(0))] + hundred;
+        number = number % 100;
+        numStr = number.toString();
+    }
+
+    if (number < 20) {
+        result += arrOnes[number];
+    } else {
+        result += arrTens[parseInt(numStr.charAt(0))];
+        result += arrOnes[parseInt(numStr.charAt(1))];
+    }
+    return result.trim();
+};
